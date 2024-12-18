e9533b200e7a78a7a079149c841b876171c665ca
Raphael Höser <raphael@hoeser.info>
2024-12-18 16:33:46 +0100

    Change name to Vereinsanmeldung
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- frontend/src/ts/components/elements/cup-context-user.ts
+++ frontend/src/ts/components/elements/cup-context-user.ts
@@ -105,7 +105,8 @@ export default class CupContextUser extends LitElement {
       : html`<cup-centered-icon-box>
           <form @submit=${this.login}>
             <h1>NRW Freestyle Cup 2025</h1>
-            <h2>Traineranmeldung</h2>
+            <h2>Vereinsanmeldung</h2>
+            <p>Die Anmeldung erfolgt vereinsweise. Bitte nur einen Nutzer pro Verein anlegen.</p>
             ${this.error ? html`<p id="error">${this.error}</p>` : nothing}
             <label>
               Email
